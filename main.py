c807266ef29b42c33990560f1fff7c62fbcf751c
acnaumann <alysson12985@gmail.com>
2019-11-26 22:11:44 -0600

    added content to templates

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask, redirect, request, render_template, session
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, request, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
-#app.secret_key = 
+
 
 
 
@@ -16,11 +17,17 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(5000))
+    new_post = db.Column(db.Boolean)
 
-    # def __init__(self, ):
-    #     self.
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
 
 
-@app.route('/main-page', methods=['POST', 'GET'])
-def blogs():
-    return render_template('blog.html')
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('blog.html', title="Build A Blog")
